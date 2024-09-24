6dee4318389ffa1ae1961d1cf5696642594a0abf
Keelan Farren <Farren-K3@ulster.ac.uk>
2024-09-24 14:55:03 +0100

    added choice of operator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,41 @@
 ﻿Console.WriteLine("Type in the first number followed by the enter key");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Type in the second number followed by the enter key");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-int result = firstNumber + secondNumber;
+int result;
 
-Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+Console.WriteLine("Choose from the following options");
+Console.WriteLine("1 - Add");
+Console.WriteLine("2 - Subtract");
+Console.WriteLine("3 - Divide");
+Console.WriteLine("4 - Multiply");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+if (choice == 1)
+{
+    result = firstNumber + secondNumber;
+    Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+}
+else if (choice == 2)
+{
+    result =firstNumber - secondNumber;
+    Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, result);
+}
+else if (choice == 3)
+{
+    result = firstNumber / secondNumber;
+    Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
+    
+}
+else if (choice == 4)
+{
+    result = firstNumber * secondNumber;
+    Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, result);
+}
+else
+{
+    Console.WriteLine("Invalid Number");
+}
+
